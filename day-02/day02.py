9090211ff23e5fecabe3a2bfe646189233900d43
kadeguilherme <kadeguilherme@gmail.com>
2022-12-07 11:18:25 -0300

    Day02 parte 02 concluido

--- day-02/day02.py
+++ day-02/day02.py
@@ -30,3 +30,35 @@ while linhas:
         totalscore += 3 + 3
     linhas = arq.readline()
 print(totalscore)
+
+# PARTE 2
+
+arq = open("day02.txt", "r")
+linhas = arq.readline()
+totalscoredesired = 0
+
+while linhas:
+    linha = linhas.split()
+    if linha[0] == 'A' and linha[1] == 'X':
+        totalscoredesired += 3
+    elif linha[0] == 'A' and linha[1] == 'Y':
+        totalscoredesired += 1 + 3
+    elif linha[0] == 'A' and linha[1] == 'Z':
+        totalscoredesired += 2 + 6
+
+    if linha[0] == 'B' and linha[1] == 'X':
+        totalscoredesired += 1
+    elif linha[0] == 'B' and linha[1] == 'Y':
+        totalscoredesired += 2 + 3
+    elif linha[0] == 'B' and linha[1] == 'Z':
+        totalscoredesired += 3 + 6
+
+    if linha[0] == 'C' and linha[1] == 'X':
+        totalscoredesired += 2
+    elif linha[0] == 'C' and linha[1] == 'Y':
+        totalscoredesired += 3 + 3
+    elif linha[0] == 'C' and linha[1] == 'Z':
+        totalscoredesired += 1 + 6
+    linhas = arq.readline()
+
+print(totalscoredesired)
