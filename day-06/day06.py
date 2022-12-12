b1e0bc73cb926122a0b9930235f3baa09cbda330
kadeguilherme <kadeguilherme@gmail.com>
2022-12-12 10:24:59 -0300

    Parte 2 Day 06 Concluido

--- day-06/day06.py
+++ day-06/day06.py
@@ -17,3 +17,23 @@ with open("day06.txt", "r") as f:
             break
         position_characters += 1
 # BUG lista_characters.append(arq[0][position_current]) IndexError: string index out of range
+
+
+with open("day06.txt", "r") as f:
+    arq = f.readlines()
+    position_current = 0
+    list_characters = []
+    position_characters = 0
+    for i in arq[0]:
+        list_characters = []
+        position_current = position_characters
+
+        for d in range(14):
+            list_characters.append(arq[0][position_current])
+            position_current += 1
+
+        if len(set(list_characters)) == 14:
+            print(position_current)
+            break
+        position_characters += 1
+# BUG lista_characters.append(arq[0][position_current]) IndexError: string index out of range
