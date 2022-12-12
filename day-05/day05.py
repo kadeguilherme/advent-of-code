30b83c845b88bf0249fc579a85f62a722bcba453
kadeguilherme <kadeguilherme@gmail.com>
2022-12-11 23:36:50 -0300

    Parte 2 Day05 Concluido.

--- day-05/day05.py
+++ day-05/day05.py
@@ -25,3 +25,35 @@ with open("day05.txt", 'r') as arq:
 for stack in stacks:
     print(stacks[stack][-1], end="")
 print("")
+
+# PARTE 2
+with open("day05.txt", 'r') as arq:
+
+    index_strings, instrucs = [i.splitlines()
+                               for i in arq.read().strip('\n').split('\n\n')]
+    stacks = {int(digit): [] for digit in index_strings[-1].replace(" ", "")}
+    # Estou pegando a posição que se encontra os numero: 1,2,3, .. 9 no array
+    position_stacks = [index for index, char in enumerate(
+        index_strings[-1]) if char != ' ']
+
+    # Percorrendo o index_strings e position_stacks verifico se na string[index] está vazio, caso contrario possui uma string e add
+    # no dicionario
+    for string in index_strings[:-1]:
+        num = 1
+        for index in position_stacks:
+            if string[index] != " ":
+                stacks[num].insert(0, string[index])
+            num += 1
+
+    for instruc in instrucs:
+        quant, stack1, stack2 = int(instruc.split(' ')[1]), int(instruc.split(
+            ' ')[3]), int(instruc.split(' ')[-1])
+        create_to_remove = stacks[stack1][-quant:]
+        # removendondo as create_to_remove da stacks autal
+        stacks[stack1] = stacks[stack1][:-quant]
+        for i in create_to_remove:
+            stacks[stack2].append(i)
+
+for stack in stacks:
+    print(stacks[stack][-1], end="")
+print("")
