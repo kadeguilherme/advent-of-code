3d4a1588a3cdb1a9e521f9ba5e82acbb529de3ea
kadeguilherme <kadeguilherme@gmail.com>
2022-12-14 00:18:38 -0300

    Conseguir separar os dir e sub e adicionar na lista dirs os diretorios.

---
+++ day-07/day07.py
@@ -0,0 +1,23 @@
+import os
+with open("day07.txt", "r") as file:
+    data = file.read().strip().split("\n")
+
+subdirs = {}
+directory_size = {}
+
+for line in data:
+    if line[0] == '$':
+        ds, cmd, *dir = line.split()
+        if cmd == 'cd':
+            path = dir[0]
+            if path == '/':
+                curdir = path
+            else:
+                #print(os.path.join(curdir, path))
+                curdir = os.path.normpath((os.path.join(curdir, path)))
+                # print(curdir)
+            if curdir not in subdirs:
+                subdirs[curdir] = []
+    else:
+        print(line.split())
+print(subdirs)
